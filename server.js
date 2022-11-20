b044aace62a55bb0689d7992b7b16e160f3553d6
root <root@kalip.kalis>
2022-11-20 13:20:49 -0500

    payment intent created on backend

--- server.js
+++ server.js
@@ -1,16 +1,43 @@
 const express = require('express')
 const app = express()
 
+// JSON
+app.use(express.json())
+
+// dotenv for accesing .env file.
+require('dotenv').config()
+
 // getting stripe here and attaching the secret test key, node will attach that key to all requests to stripe API.
-const stripe = require("stripe")()
+const stripe = require("stripe")(process.env.SECRET_KEY)
+
 // This is the basic home route.
 app.get("/", (req, res)=>{
 	res.status(200).send("This is the homepage")
 })
 
+// this is where your business logic goes, you will get an item list from front-end that will
+// contain ids of items your website sells, you will cross check their prices from the database
+// and eventually pass the total amount to be paid.
+const calculateAmount = (items)=>{
+	// this is demo logic.
+	const dummyPrice = 100
+
+	return dummyPrice*items;
+}
+
 // This is the route to create payment intent
 app.post("/create-payment-intent", async(req, res)=>{
-	res.status(200).send("This is the payment page")
+	const {currency} = req.body // what do you want to buy ? and with what currency.
+
+	const paymentIntent = await stripe.paymentIntents.create({ // this function creates the payment intent
+		amount: calculateAmount(10),
+		currency: currency,
+		payment_method_types: ['card'] // we only support card payment at the moment.
+	})
+
+	res.status(200).send({
+		clientSecret: paymentIntent.client_secret,
+	})
 })
 
 app.listen(9090, ()=>{
